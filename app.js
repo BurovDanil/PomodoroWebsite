15c3f6caf16884eff8fd6cf1de4f29d79cc46534
BurovDanil <danilburov67@gmail.com>
2024-05-13 00:15:30 +0200

    added multiple times to start from, started implementing the functionality

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const message = document.querySelector('.message');
 const minutesDisplay = document.querySelector('.minutes');
 const secondsDisplay = document.querySelector('.seconds');
 
+const firstMinButton = document.querySelector('.btn-30');
+const secondMinButton = document.querySelector('.btn-25');
+const thirdMinButton = document.querySelector('.btn-20');
+
 const sessionMin = document.querySelector('.minutes');
 // const sessionSec = document.querySelector('.seconds');
 let interval;
@@ -17,13 +21,14 @@ let isTimerRunning = false; //when the application is running -> flag variablex
 let isTimerStopped = false;
 let totalSeconds;
 
-//we can add an argument to the initialiseTimer in order to prevent when restarting 24:30 when restart to get 24
+//we can add an a`rgument to the initialiseTimer in order to prevent when restarting 24:30 when restart to get 24
 //as well as initialise a html variable and it is going to be a field, get the data from the field and feed it to the method
 //to have customized pomodoro timer
 
 const initialiseTimer = () => {
     const sessionAmount = Number.parseInt(sessionMin.textContent);
-    totalSeconds = (sessionAmount + 1)  * 60; //Temporary solution for resTimer() method!
+    totalSeconds = sessionAmount * 60;
+    // totalSeconds = (sessionAmount + 1)  * 60; //Temporary solution for resTimer() method!
 }
 
 const appTimer = () => {
@@ -66,6 +71,7 @@ const resTimer = () => {
     initialiseTimer();
     updateDisplay();
     isTimerRunning = false;
+    sessionMin.textContent = 0;
 }
 const breakTimer = () => {
 
@@ -76,6 +82,18 @@ const changeMessage = (text) => {
         message.textContent = '';
     }, 1000);
 }
+const setTimer = (textContent) => {
+    sessionMin = textContent;
+    initialiseTimer();
+    updateDisplay();
+}
+    //what I want to do is get the value from the html and set it to the totalSeconds
+    //instead of having a fixed value
+
+firstMinButton.addEventListener('click', () => setTimer(firstMinButton.textContent));
+secondMinButton.addEventListener('click', setTimer(secondMinButton.textContent));
+thirdMinButton.addEventListener('click', setTimer(thirdMinButton.textContent));
+
 startButton.addEventListener('click', appTimer);
 restartButton.addEventListener('click', resTimer);
 stopButton.addEventListener('click', stopTimer);
